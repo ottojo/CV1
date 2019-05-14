5f8df7c9e92767ebdf0c9c19698d8d564f5821ea
ottojo <jottosmail@gmail.com>
2019-05-14 20:43:58 +0200

    plot annotations

--- Exercise1/sh01ex01.m
+++ Exercise1/sh01ex01.m
@@ -8,20 +8,24 @@ imshow(obstA);
 title("obstA.png");
 
 subplot(2,2,2);
-plot(x, histA);
+plot(histA);
 xlim([0 255]);
 title("Histogram: obstA");
 xlabel("Pixel value");
 ylabel("frequency");
+text(75, 0.003, "\leftarrow Apple");
+text(110, 0.019, "\leftarrow Orange");
+text(150, 0.02, "\leftarrow Background");
 
 subplot(2,2,3);
 imshow(obstB);
 title("obstB.png");
 
-
 subplot(2,2,4);
-plot(x, histB);
+plot(histB);
 xlim([0 255]);
 title("Histogram: obstB");
 xlabel("Pixel value");
 ylabel("frequency");
+text(70, 0.007, "\leftarrow Apples");
+text(145, 0.022, "\leftarrow Background");
