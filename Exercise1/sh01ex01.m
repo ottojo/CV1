a1e20dd5344e1801588a06046787bdb61b9950bf
ottojo <jottosmail@gmail.com>
2019-05-14 21:07:22 +0200

    add task 1.5

--- Exercise1/sh01ex01.m
+++ Exercise1/sh01ex01.m
@@ -3,11 +3,11 @@ obstB = imread("obstB.png");
 histA = myHistogram(obstA);
 histB = myHistogram(obstB);
 
-subplot(2,2,1);
+subplot(4,2,1);
 imshow(obstA);
 title("obstA.png");
 
-subplot(2,2,2);
+subplot(4,2,2);
 plot(histA);
 xlim([0 255]);
 title("Histogram: obstA");
@@ -17,11 +17,11 @@ text(75, 0.003, "\leftarrow Apple");
 text(110, 0.019, "\leftarrow Orange");
 text(150, 0.02, "\leftarrow Background");
 
-subplot(2,2,3);
+subplot(4,2,3);
 imshow(obstB);
 title("obstB.png");
 
-subplot(2,2,4);
+subplot(4,2,4);
 plot(histB);
 xlim([0 255]);
 title("Histogram: obstB");
@@ -29,3 +29,22 @@ xlabel("Pixel value");
 ylabel("frequency");
 text(70, 0.007, "\leftarrow Apples");
 text(145, 0.022, "\leftarrow Background");
+
+
+subplot(4,2,5);
+plot(obstA(50,:));
+title("obstA row 50");
+
+subplot(4,2,6);
+plot(obstA(200,:));
+title("obstA row 200");
+
+subplot(4,2,7);
+plot(obstB(50,:));
+title("obstB row 50");
+
+subplot(4,2,8);
+plot(obstB(200,:));
+title("obstB row 200");
+
+set(gcf, 'Position', [100 100 1000 800])
