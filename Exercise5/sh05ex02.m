429aa768aa545f240a0357b193516db167cceb93
ottojo <jottosmail@gmail.com>
2019-07-18 13:10:45 +0200

    Add sh05ex02_2_1 (calculation of L pyramid)

---
+++ Exercise5/sh05ex02.m
@@ -0,0 +1,21 @@
+image = im2double(imread('boats.tif'));
+n = 6;
+G = gaussianPyramid(image, n, 1);
+
+for i = 1:n
+  % calc L{i}
+  if i ~= n
+    L{i} = G{i} - imresize(G{i+1}, 2);
+    else
+      L{i} = G{i};
+    endif
+endfor
+
+%% Plot L
+figure('Name', 'Laplacian Pyramid');
+for i = 1:n
+  subplot(3,n/3,i);
+  imshow(L{i}, []);
+  colorbar();
+  title(['L(' num2str(i) ')']);
+endfor
