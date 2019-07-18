2166eed57cb54231abd33c259c486ff1d2c70475
ottojo <jottosmail@gmail.com>
2019-07-18 16:56:27 +0200

    add error calculation

--- Exercise5/sh05ex02.m
+++ Exercise5/sh05ex02.m
@@ -52,3 +52,17 @@ clear G;
 % Reconstruct image from L
 figure('Name', 'Reconstructed Image from L after Compression');
 imshow(reconstruct(L),[]);
+
+lambda = 0:0.05:0.3
+error = zeros(1, size(lambda,2));
+
+for i = 1:size(lambda,2)
+  LT = L;
+  for k = 1:n-1
+    LT{k}(abs(LT{k}) < lambda(i) * max(max(abs(LT{k})))) = 0;
+  endfor
+  error(i) = immse(image, reconstruct(LT));
+endfor
+
+figure('Name', 'Error');
+plot(lambda, error)
