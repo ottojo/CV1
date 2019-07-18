fc71eca5910396da5d172201db65c7f75deb0f28
ottojo <jottosmail@gmail.com>
2019-07-18 13:42:11 +0200

    adds sh05ex02_3 (compression)

--- Exercise5/sh05ex02.m
+++ Exercise5/sh05ex02.m
@@ -29,3 +29,26 @@ clear G;
 % Reconstruct image from L
 figure('Name', 'Reconstructed Image from L');
 imshow(reconstruct(L),[]);
+
+% Compression
+lambda = 0.2;
+for i = 1:n-1
+  L{i}(abs(L{i}) < lambda * max(max(abs(L{i})))) = 0;
+endfor
+
+%% Plot L
+figure('Name', 'Laplacian Pyramid after Compression');
+for i = 1:n
+  subplot(3,n/3,i);
+  imshow(L{i}, []);
+  colorbar();
+  title(['L(' num2str(i) ')']);
+endfor
+
+
+% Reset G
+clear G;
+
+% Reconstruct image from L
+figure('Name', 'Reconstructed Image from L after Compression');
+imshow(reconstruct(L),[]);
